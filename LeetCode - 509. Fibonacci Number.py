c6c6ac9d90b516fb67b27cefbb2a9d7ff8421dd7
Jean Ferrer <jeanferrerdata@gmail.com>
2026-05-14 11:16:39 -0300

    feat: add new exercise

---
+++ LeetCode - 509. Fibonacci Number.py
@@ -0,0 +1,18 @@
+# 2026-05-14
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+
+        if n == 0:
+            return 0
+        elif n ==1:
+            return 1
+
+        self.memo[n] = self.fib(n-1) + self.fib(n-2)
+
+        return self.memo[n]
